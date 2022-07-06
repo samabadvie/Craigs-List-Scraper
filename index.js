ccbe465b396b19e7bd300bd16a54a1d4345c3f81
samabadvie <samabadvie@gmail.com>
2022-07-06 18:33:49 +0430

    Initial Puppeteer to google.com

---
+++ index.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+async function main(){
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto("https://google.com");
+}
+
+main();
